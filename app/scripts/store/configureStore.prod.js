08a4bb3159ab08b6eb9e1c530d2b1dd796cebe32
Gil Barbara <gilbarbara@gmail.com>
2016-01-02 18:50:00 -0200

    add envify

--- app/scripts/store/configureStore.prod.js
+++ app/scripts/store/configureStore.prod.js
@@ -1,12 +1,10 @@
-import { applyMiddleware, createStore, combineReducers, compose } from 'redux';
+import { applyMiddleware, createStore, combineReducers } from 'redux';
 import { routeReducer } from 'redux-simple-router';
 import thunk from 'redux-thunk';
 import { apiMiddleware } from 'redux-api-middleware';
 import rootReducer from '../reducers';
 
-const createStoreWithMiddleware = compose(
-	applyMiddleware(thunk, apiMiddleware)
-)(createStore);
+const createStoreWithMiddleware = applyMiddleware(thunk, apiMiddleware)(createStore);
 
 const reducer = combineReducers(Object.assign({}, rootReducer, {
 	routing: routeReducer
