46ffdccbe7f640c97becd1f0345617ab65970143
Gil Barbara <gilbarbara@gmail.com>
2016-01-06 01:09:20 -0200

    remove flux from Footer

--- app/scripts/components/elements/Footer.jsx
+++ app/scripts/components/elements/Footer.jsx
@@ -16,23 +16,31 @@ class Footer extends React.Component {
 					<h3>Powered by:</h3>
 
 					<ul className="tech list-unstyled">
-						<li><a href="http://facebook.github.io/react/" target="_blank">
-							<img src="http://svgporn.com/logos/react.svg" width="96" />
-						</a></li>
-						<li><a href="https://facebook.github.io/flux/" target="_blank">
-							<img src="http://svgporn.com/logos/flux.svg" width="96" />
-						</a></li>
-						<li><a href="http://gulpjs.com/" target="_blank">
-							<img src="http://svgporn.com/logos/gulp.svg" width="64" />
-						</a></li>
-						<li><a href="http://browserify.org/" target="_blank">
-							<img src="http://svgporn.com/logos/browserify-icon.svg" width="96" />
-						</a></li>
-						<li><a href="https://babeljs.io/" target="_blank">
-							<img src="http://svgporn.com/logos/babel.svg" width="96" /></a></li>
-						<li><a href="http://mochajs.org/" target="_blank">
-							<img src="http://svgporn.com/logos/mocha.svg" width="64" />
-						</a></li>
+						<li>
+							<a href="http://facebook.github.io/react/" target="_blank">
+								<img src="http://svgporn.com/logos/react.svg" width="96" />
+							</a>
+						</li>
+						<li>
+							<a href="http://browserify.org/" target="_blank">
+								<img src="http://svgporn.com/logos/browserify-icon.svg" width="96" />
+							</a>
+						</li>
+						<li>
+							<a href="http://gulpjs.com/" target="_blank">
+								<img src="http://svgporn.com/logos/gulp.svg" width="52" />
+							</a>
+						</li>
+						<li>
+							<a href="https://babeljs.io/" target="_blank">
+								<img src="http://svgporn.com/logos/babel.svg" width="96" />
+							</a>
+						</li>
+						<li>
+							<a href="http://mochajs.org/" target="_blank">
+								<img src="http://svgporn.com/logos/mocha.svg" width="64" />
+							</a>
+						</li>
 					</ul>
 				</div>
 			</footer>
