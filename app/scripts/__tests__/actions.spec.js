1906f9f08156655c29902dfe84229934bca25437
Gil Barbara <gilbarbara@gmail.com>
2016-03-13 02:03:41 -0300

    split action tests in 2 files

--- app/scripts/__tests__/actions.spec.js
+++ app/scripts/__tests__/actions.spec.js
@@ -1,20 +1,13 @@
 import expect from 'expect';
-import configureMockStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
-import { apiMiddleware } from 'redux-api-middleware';
-import nock from 'nock';
 
 import * as Actions from '../actions';
 import { ActionTypes } from '../constants';
 
-const middlewares = [thunk, apiMiddleware];
-const createMockStore = configureMockStore(middlewares);
-
 describe('Actions', () => {
   describe('goTo', () => {
     it('should create an action to navigate with react-router > UPDATE_PATH', () => {
-      expect([Actions.goTo('/destination', 'params', 'query=1')])
-        .toInclude('/destination/params?query=1', (a, b) => a.arg === b);
+      expect([Actions.goTo('/destination')])
+        .toInclude('/destination', (a, b) => a.payload.args[0] === b);
     });
   });
 
@@ -41,39 +34,5 @@ describe('Actions', () => {
 
       expect(Actions.hideAlert('error')).toEqual(expectedAction);
     });
-  });
-});
-
-describe('Async Actions', () => {
-  afterEach(() => {
-    nock.cleanAll();
-  });
-
-  it(`should dispatch ${ActionTypes.POPULAR_SUCCESS} when fetching popular has been done`, (done) => {
-    nock('https://api.hypem.com')
-      .get('/v2/popular')
-      .reply(200, { payload: 'OK!' });
-
-    const expectedActions = [
-      { type: ActionTypes.POPULAR_REQUEST, payload: undefined, meta: undefined },
-      { type: ActionTypes.POPULAR_SUCCESS, payload: { payload: 'OK!' }, meta: undefined }
-    ];
-
-    const store = createMockStore({ payload: 'OK!' }, expectedActions, done);
-    store.dispatch(Actions.fetchPopular());
-  });
-
-  it(`should dispatch ${ActionTypes.LASTWEEK_SUCCESS} when fetching popular has been done`, (done) => {
-    nock('https://api.hypem.com')
-      .get('/v2/popular?mode=lastweek')
-      .reply(200, { payload: 'OK!' });
-
-    const expectedActions = [
-      { type: ActionTypes.LASTWEEK_REQUEST, payload: undefined, meta: undefined },
-      { type: ActionTypes.LASTWEEK_SUCCESS, payload: { payload: 'OK!' }, meta: undefined }
-    ];
-
-    const store = createMockStore({ payload: 'OK!' }, expectedActions, done);
-    store.dispatch(Actions.fetchLastWeek());
   });
 });
---
+++ app/scripts/__tests__/async-actions.spec.js
@@ -0,0 +1,52 @@
+import expect from 'expect';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { apiMiddleware } from 'redux-api-middleware';
+import nock from 'nock';
+
+import * as Actions from '../actions';
+import { ActionTypes } from '../constants';
+
+const middlewares = [thunk, apiMiddleware];
+const createMockStore = configureMockStore(middlewares);
+
+describe('Async Actions', () => {
+  afterEach(() => {
+    nock.cleanAll();
+  });
+
+  it(`should dispatch ${ActionTypes.POPULAR_SUCCESS} when fetching popular has been done`, (done) => {
+    nock('https://api.hypem.com')
+      .get('/v2/popular')
+      .reply(200, { payload: 'OK!' });
+
+    const expectedActions = [
+      { type: ActionTypes.POPULAR_REQUEST, payload: undefined, meta: undefined },
+      { type: ActionTypes.POPULAR_SUCCESS, payload: { payload: 'OK!' }, meta: undefined }
+    ];
+
+    const store = createMockStore({});
+    store.dispatch(Actions.fetchPopular())
+      .then(() => {
+        expect(store.getActions()).toEqual(expectedActions);
+        done();
+      });
+  });
+
+  it(`should dispatch ${ActionTypes.LASTWEEK_SUCCESS} when fetching popular has been done`, (done) => {
+    nock('https://api.hypem.com')
+      .get('/v2/popular?mode=lastweek')
+      .reply(200, { payload: 'OK!' });
+
+    const expectedActions = [
+      { type: ActionTypes.LASTWEEK_REQUEST, payload: undefined, meta: undefined },
+      { type: ActionTypes.LASTWEEK_SUCCESS, payload: { payload: 'OK!' }, meta: undefined }
+    ];
+
+    const store = createMockStore({});
+    store.dispatch(Actions.fetchLastWeek()).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+      done();
+    });
+  });
+});
